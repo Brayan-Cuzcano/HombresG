622da184073c949ca0c5b95c0f2b8fc55f3d51a3
Alex-S-J <66312623+Alex-S-J@users.noreply.github.com>
2020-06-29 12:46:35 -0500

    interfaz mejorada

--- nuevainterfaz.py
+++ nuevainterfaz.py
@@ -1,48 +1,44 @@
-
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
+from datetime import date
+import Temperaturas
 
 lista = []
 
 def guardar():
-    n =nombre.get()
-    ap=app.get()
-    am=apm.get()
-    c=correo.get()
-    t=telefono.get()
-    lista.append(n+"$"+ap+"$"+am+"$"+c+"$"+t)
+    Temperatura=e1.get()
+    Fecha=e2.get()
+    
+    lista.append(Temperatura+"$"+Fecha)
     escribirContacto()
-    messagebox.showinfo("Guardado","Pasiente guardado")
-    nombre.set("")
-    app.set("")
-    telefono.set("")
-    correo.set("")
+    messagebox.showinfo("Guardado","Información del paciente a sido guardado.")
+    
     consultar()
 def eliminar():
     eliminado=conteliminar.get()
     removido=False
     for elemento in lista:
         arreglo=elemento.split("$")
-        if (conteliminar.get())==arreglo[3]:
+        if (conteliminar.get())==arreglo[0]:
             lista.remove(elemento)
             removido=True
     escribirContacto()
     consultar()
     if removido:
-        messagebox.showinfo("Eliminar","Elemento eliminado"+eliminado)
+        messagebox.showinfo("Eliminar","Informacion del paciente a sido eliminado."+eliminado)
 def consultar():
     r=Text(ventana,width=80,height=15)
     lista.sort()
     valores=[]
-    r.insert(INSERT,"Nombre\t\tApellido P\t\tApellido M\t\t Telefono\t\tcorreo\n")
+    r.insert(INSERT,"Fecha\t\tTemperatura\n")
     for elemento in lista:
         arreglo = elemento.split("$")
-        valores.append(arreglo[3])
-        r.insert(INSERT,arreglo[0]+"\t\t"+arreglo[1]+"\t\t"+arreglo[2]+"\t\t"+arreglo[3]+"\t\t"+arreglo[4]+"\t\n")
-    r.place(x=20,y=230)
-    if lista==[]:
-        spinTelefono=Spinbox(ventana,value=(valores)).place(x=450,y=50)
+        #valores.append(arreglo[1])
+        r.insert(INSERT,arreglo[1]+"\t\t"+arreglo[0]+"\n")
+    r.place(x=20,y=130)
+    #if lista==[]:
+    #    spinTelefono=Spinbox(ventana,value=(valores)).place(x=250,y=20)
     r.config(state=DISABLED)
 def iniciarArchivo():
     archivo=open("oh.txt","a")
@@ -67,11 +63,12 @@ def escribirContacto():
 
 
 ventana =Tk()
-nombre=StringVar()
-app=StringVar()
-apm=StringVar()
-correo=StringVar()
-telefono=StringVar()
+Fecha=StringVar()
+Temperatura=StringVar()
+#app=StringVar()
+#apm=StringVar()
+#correo=StringVar()
+#telefono=StringVar()
 conteliminar=StringVar()
 
 cargar()
@@ -81,24 +78,71 @@ consultar()
 
 colorFondo="#006"
 colorLetra="#FFF"
-ventana.title("Interfaz Grafica - HombreG productions :v")
+ventana.title("IMSOMNIA - Hombres G")
 ventana.geometry("700x500")
 ventana.configure(background=colorFondo)
 
 #Etiquetas Creacion
-etiquetaTitulo =Label(ventana,text="Bienvenidos",bg=colorFondo,fg=colorLetra,font=("Helvetica",16)).place(x=230,y=10)
-etiquetaN = Label(ventana,text="Nombre: ",bg=colorFondo,fg=colorLetra).place(x=50,y=50)
-cajaN = tk.Entry(ventana,textvariable=nombre).place(x=150,y=50)
-etiquetaApp = Label(ventana,text="Apellido Paterno: ",bg=colorFondo,fg=colorLetra).place(x=50,y=80)
-cajaApp =tk.Entry(ventana,textvariable=app).place(x=150,y=80)
-etiquetaApm = Label(ventana, text="Apellido Materno: ",bg=colorFondo,fg=colorLetra  ).place(x=50,y=110)
-cajaApm = tk.Entry(ventana, textvariable=apm).place(x=150, y=110)
-etiquetaT = Label(ventana,text="Telefono: ",bg=colorFondo,fg=colorLetra).place(x=50,y=140)
-cajaT = tk.Entry(ventana,textvariable=telefono).place(x=150,y=140)
-etiquetaC = Label(ventana, text="Correo: ", bg=colorFondo, fg=colorLetra).place(x=50,y=170)
-cajaC=tk.Entry(ventana,textvariable=correo).place(x=150,y=170)
-etiquetaEliminar = Label(ventana, text="Telefono: ",bg=colorFondo,fg=colorLetra).place(x=370,y=50)
-spinTelefono = Spinbox(ventana,textvariable=conteliminar).place(x=450,y=50)
-botonGuardar= Button(ventana,text="Guardar",command=guardar,bg="#009",fg="White").place(x=180,y=200)
-botonEliminar=Button(ventana,text="Eliminar",command=eliminar,bg="#009",fg="White").place(x=470,y=80)
+
+##etiquetaTitulo =Label(ventana,text="Bienvenidos",bg=colorFondo,fg=colorLetra,font=("Helvetica",16)).place(x=295,y=10)
+##etiquetaN = Label(ventana,text="Temperatura : ",bg=colorFondo,fg=colorLetra).place(x=50,y=50)
+#cajaN = tk.Entry(ventana,textvariable=nombre).place(x=150,y=50)
+##etiquetaApp = Label(ventana,text="Fecha : ",bg=colorFondo,fg=colorLetra).place(x=50,y=80)
+#cajaApp =tk.Entry(ventana,textvariable=app).place(x=150,y=80)
+##Prue=tk.Entry(ventana.insert(0, "¡Hola, mundo!")).place(x=150,y=80)
+#today = date.today()
+#print("Today's date:", today)
+#Entry.insert(0, "¡Hola, mundo!")
+
+##cajaApp.insert(0,'Hola mundo')
+
+#app="holamundo"
+#entry = tk.Entry(ventana)
+# Posicionarla en la ventana.
+#entry.place(x=150, y=100)
+
+
+
+tk.Label(ventana, text="Temperatura").place(x=50,y=80)        #grid(row=0)
+tk.Label(ventana, text="Fecha").place(x=50,y=100)         #grid(row=1)
+##e1 es temperatura
+##e2 es fecha
+e1 = tk.Entry(ventana)
+e2 = tk.Entry(ventana)
+
+today = date.today()
+#recordar
+Fecha = str(today.day) + "-" + str(today.month) + "-" + str(today.year)
+print("Today's date:", today)
+e2.insert(10, Fecha)
+
+
+
+
+
+e1.place(x=130,y=80)
+e2.place(x=130,y=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#etiquetaApm = Label(ventana, text="Temperatura 3 : ",bg=colorFondo,fg=colorLetra  ).place(x=50,y=110)
+#cajaApm = tk.Entry(ventana, textvariable=apm).place(x=150, y=110)
+#etiquetaT = Label(ventana,text="Temperatura 4 : ",bg=colorFondo,fg=colorLetra).place(x=50,y=140)
+#cajaT = tk.Entry(ventana,textvariable=telefono).place(x=150,y=140)
+#etiquetaC = Label(ventana, text="Temperatura 5 : ", bg=colorFondo, fg=colorLetra).place(x=50,y=170)
+#cajaC=tk.Entry(ventana,textvariable=correo).place(x=150,y=170)
+#etiquetaEliminar = Label(ventana, text="Telefono: ",bg=colorFondo,fg=colorLetra).place(x=370,y=50)
+#spinTelefono = Spinbox(ventana,textvariable=conteliminar).place(x=450,y=50)
+botonGuardar= Button(ventana,text="Guardar",command=guardar,bg="#009",fg="White").place(x=280,y=50)
+botonEliminar=Button(ventana,text="Eliminar",command=eliminar,bg="#009",fg="White").place(x=300,y=80)
 ventana.mainloop()
